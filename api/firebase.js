a8ffeea03587db78d250cea3693a21e4f79d9c43
leonghongfai <leonghongfai@gmail.com>
2021-05-26 17:13:41 +0800

    bare framework

---
+++ api/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBKwaiPrr_ENZdkyPaTLkDN-owQn63CSuk",
+    authDomain: "orbital-21837.firebaseapp.com",
+    projectId: "orbital-21837",
+    storageBucket: "orbital-21837.appspot.com",
+    messagingSenderId: "685879920678",
+    appId: "1:685879920678:web:4eef086d8a3721c117fee7",
+    measurementId: "G-XHCD2EP5HK",
+    databaseURL: "https://orbital-21837-default-rtdb.asia-southeast1.firebasedatabase.app"
+  };
+  // Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
