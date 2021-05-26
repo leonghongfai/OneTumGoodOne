daa3b1183a97f5c3c52bc6a7e1880618c4eae3d5
leonghongfai <leonghongfai@gmail.com>
2021-05-27 03:51:35 +0800

    Added Register Screen UI

---
+++ src/screens/RegisterScreen/RegisterScreen.js
@@ -0,0 +1,58 @@
+import { Assets } from '@react-navigation/stack';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View, Image, TextInput, TouchableOpacity,SafeAreaView, ScrollView } from 'react-native';
+import Styles from './Styles'
+import { useState } from 'react'
+
+
+const RegisterScreen = (props) => {
+    const [text, setText] = useState('')
+    const [pass, setPass] = useState('')
+    const [pass2, setPass2] = useState('')
+    const [view, setView] = useState(false)
+
+    return (
+            <View style = {Styles.container}>
+                <View style={Styles.topContainer} />
+                <Text style={Styles.title}>REGISTER</Text>
+                <Image 
+                    style={Styles.logo} 
+                    source = {require('../../../assets/favicon.png')}
+                />
+                <TextInput
+                    style= {Styles.userInput}
+                    value = {text}
+                    onChangeText= {text => setText(text)}
+                    autoCapitalize = 'none'
+                    placeholder= 'Enter Email'
+                />
+                 <TextInput
+                    style= {Styles.userInput}
+                    value = {pass}
+                    onChangeText= {(text) => setPass(text)}
+                    autoCapitalize = 'none'
+                    placeholder = 'Enter Password'
+                    secureTextEntry
+                />
+                <TextInput
+                   style= {Styles.userInput}
+                   value = {pass2}
+                   onChangeText= {(pass2) => setPass2(pass2)}
+                   autoCapitalize = 'none'
+                   placeholder = 'Enter Password Again'
+                   secureTextEntry
+               />
+                <TouchableOpacity style={Styles.logInButton}>
+                    <Text style={Styles.logInText}>Log In</Text>
+                </TouchableOpacity>  
+                <View style={Styles.noAccount}>
+                    <Text style={Styles.noAccountWording}>Already one of us? <Text 
+                        style= {Styles.registerWording}
+                        >Login!</Text></Text>
+                </View>
+            </View>     
+    )
+}
+
+export default RegisterScreen;
