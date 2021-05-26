cf20b80b4e4268ddb6b4aa4606005f798a2005de
leonghongfai <leonghongfai@gmail.com>
2021-05-27 00:33:19 +0800

    login screen ui

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -1,13 +1,52 @@
+import { Assets } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image, TextInput, TouchableOpacity,SafeAreaView, ScrollView } from 'react-native';
 import LoginScreenStyles from './LoginScreenStyles'
+import { useState } from 'react'
 
-LoginScreen = (props) => {
+
+const LoginScreen = (props) => {
+    const [text, setText] = useState('')
+    const [pass, setPass] = useState('')
+    const [view, setView] = useState(false)
+
     return (
-        <View style = {LoginScreenStyles.container}>
+            <View style = {LoginScreenStyles.container}>
+                <View style={LoginScreenStyles.topContainer} />
+                <Text style={LoginScreenStyles.title}>LOGIN</Text>
+                <Image 
+                    style={LoginScreenStyles.logo} 
+                    source = {require('../../../assets/favicon.png')}
+                />
+                <TextInput
+                    style= {LoginScreenStyles.userInput}
+                    value = {text}
+                    onChangeText= {text => setText(text)}
+                    autoCapitalize = 'none'
+                    placeholder= 'Email'
+                />
+                 <TextInput
+                    style= {LoginScreenStyles.userInput}
+                    value = {pass}
+                    onChangeText= {(text) => setPass(text)}
+                    autoCapitalize = 'none'
+                    placeholder = 'Password'
+                    secureTextEntry
+                />
+                <TouchableOpacity style={LoginScreenStyles.logInButton}>
+                    <Text style={LoginScreenStyles.logInText}>Log In</Text>
+                </TouchableOpacity>  
+                <View style={LoginScreenStyles.noAccount}>
+                    <Text style={LoginScreenStyles.noAccountWording}>Don't have an account? 
+                    <Text 
+                        style= {LoginScreenStyles.registerWording}
+                        >Register</Text>
+                    </Text>
+                </View>
+            </View>      
 
-        </View>    
+
 
 
     )
--- src/screens/LoginScreen/LoginScreenStyles.js
+++ src/screens/LoginScreen/LoginScreenStyles.js
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const LoginScreenStyles = StyleSheet.create({
     container: {
-        flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        flex: 1,
+        backgroundColor: 'grey',
+        paddingTop: Platform.OS === 'android'? StatusBar.currentHeight: 0,
+    },
+    topContainer:{
+        height: 70
+    },
+    title: {
+        height: 40,
+        fontSize: 25,
+        fontWeight: 'bold'
     },
     logo: {
-        flex: 1,
-        height: 90,
+        //flex: 1,
         width: 90,
+        height: 90,
+        alignSelf: "center",
+        margin: 30
+    },
+    userInput: {
+        height: 40,
+        width: 250,
+        borderRadius: 5,
+        backgroundColor: 'white',
+        marginTop: 10,
+        marginLeft: 30,
+        marginRight: 30,
+        paddingLeft: 16,
+        overflow: 'hidden',
+    },
+    logInButton: {
+        marginTop: 10,
+        height: 40,
+        width: 250,
         alignItems: 'center',
-        margin: 30,
+        justifyContent: 'center',
+        backgroundColor: '#f68817'
+    },
+    logInText: {
+        fontWeight: 'bold',
+        fontSize: 15,
+    },
+    noAccount: {
+        flex: 1,
+        alignItems: "center",
+        marginTop: 10
+    },
+    noAccountWording: {
+        fontSize: 16,
+        color: '#2e2e2d'
+    },
+    registerWording: {
+        fontSize: 16,
+        color: '#3366BB',
+        textDecorationLine: 'underline'
     }
+
 })
 
 export default LoginScreenStyles;
