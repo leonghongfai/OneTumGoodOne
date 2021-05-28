4507dade8b151dab60eb4819fb6399587c27f6e3
leonghongfai <leonghongfai@gmail.com>
2021-05-28 18:03:43 +0800

    added logout

--- src/screens/Homepage/HomeScreen.js
+++ src/screens/Homepage/HomeScreen.js
@@ -4,11 +4,27 @@ import {
     TouchableOpacity, KeyboardAvoidingView, ScrollView 
     } from 'react-native';
 import { useState } from 'react'
+import firebase from '../../../api/firebase'
+import * as Auth from '../../../api/Authentication'
 
-const HomeScreen = () => {
+
+
+const HomeScreen = (props) => {
+    //dont touch auth and handleLogOut pls
+    const handleLogOut = () => {
+        Auth.signOut(
+            () => props.navigation.navigate("Login"),
+            (error) => alert(error.message)
+        )
+    }
+
+    
     return (
         <View>
             <Text>HI</Text>
+            <TouchableOpacity onPress= {handleLogOut}>
+                <Text style={{alignSelf: 'center'}}>Log Out</Text>
+            </TouchableOpacity>
         </View>
     )
 }
