8859312c48b12e3dbe92923481dec0b7d9d8a4ac
leonghongfai <leonghongfai@gmail.com>
2021-06-01 18:32:50 +0800

    minor changes

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={'Main'} headerMode= 'none'>
+      <Stack.Navigator initialRouteName={'Login'} headerMode= 'none'>
         <Stack.Screen name="Main" component={MainScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
