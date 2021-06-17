0d10e00f202cb371fe4372cf411711521c799470
leonghongfai <leonghongfai@gmail.com>
2021-06-17 14:02:02 +0800

    redux shit

---
+++ redux/reducers/user.js
@@ -0,0 +1,13 @@
+import { USER_STATE_CHANGE } from "../constants"
+
+const initialState = {
+    currentUser: null
+}
+
+export const user = (state = initialState, action) => {
+    return {
+        ...state,
+        currentUser: action.currentUser
+    }
+
+}
