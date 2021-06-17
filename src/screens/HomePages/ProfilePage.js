185019279ad216487a155cb39f29182bcb004a47
leonghongfai <leonghongfai@gmail.com>
2021-06-17 20:01:24 +0800

    added user posts ui

--- src/screens/HomePages/ProfilePage.js
+++ src/screens/HomePages/ProfilePage.js
@@ -3,24 +3,69 @@ import {
   Text,
   View,
   Image,
-  TextInput,
-  TouchableOpacity,
-  KeyboardAvoidingView,
-  ScrollView,
-  NavigationContainer,
+  FlatList,
+  StyleSheet
 } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import ColorScheme from "../../../global/ColorScheme";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useState } from "react";
-import { render } from "react-dom";
+import firebase from 'firebase'
+require('firebase/firestore')
+import { connect } from 'react-redux'
 
-const ProfilePage = () => {
+const ProfilePage = (props) => {
+  const { currentUser, posts } = props;
+  console.log(currentUser, posts)
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Profile!</Text>
+    <View style={styles.container}>
+      <View style={styles.containerInfo}>
+        <Text>{currentUser.username}</Text>
+        <Text>{currentUser.email}</Text>
+      </View>
+
+      <View style={styles.containerGallery}>
+        <FlatList 
+          numColumns={3}
+          horizontal={false}
+          data={posts}
+          renderItem={({item}) => (
+            <View style={styles.containerImage}> 
+            <Image
+              style={styles.image}
+              source={{uri: item.downloadURL}}
+            />
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 };
 
-export default ProfilePage;
+const styles= StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 40,
+  },
+  containerInfo: {
+    margin: 20,
+  },
+  containerGallery: {
+    flex: 1,
+  },
+  image: {
+    flex:1,
+    aspectRatio: 1/1,
+  },
+  containerImage: {
+    flex: 1/3
+  },
+})
+
+const mapStateToProps = (store) => ({
+  currentUser: store.userState.currentUser,
+  posts: store.userState.posts,
+})
+
+export default connect(mapStateToProps, null)(ProfilePage);
