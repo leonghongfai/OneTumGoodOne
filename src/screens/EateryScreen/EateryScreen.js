88da9e965647b61b68d12312558ab8155936a872
Lim Wei Jie <limweijie.rcy@gmail.com>
2021-06-18 16:53:08 +0800

    FIxed warning message for FlatList among other things

--- src/screens/EateryScreen/EateryScreen.js
+++ src/screens/EateryScreen/EateryScreen.js
@@ -1,42 +1,55 @@
 import React from "react";
 import {
-	Text,
-	View,
-	SafeAreaView,
-	Image,
-	TouchableOpacity,
-	ScrollView,
-	FlatList,
+    Text,
+    View,
+    SafeAreaView,
+    Image,
+    TouchableOpacity,
+    ScrollView,
+    FlatList,
     KeyboardAvoidingView,
 } from "react-native";
+import { useNavigation } from '@react-navigation/native';
 import { icons, images } from '../../../constants'
 import Icon from 'react-native-vector-icons/Ionicons';
 import styles from "./EateryScreenStyles";
 
-const EateryScreen = (item, navigation) => {
+const EateryScreen = (item) => {
+
+    const navigation = useNavigation();
 
     const [eatery, setEatery] = React.useState(null);
 
-    React.useEffect(() => 
+    React.useEffect(() =>
         setEatery(item)
     )
 
     function renderHeader() {
-        return(
-            <View>
-                <TouchableOpacity style={styles.backBox}>
-                    <Icon name="arrow-back" size={16} color="blue" />
+        return (
+            <View style={{ flexDirection: 'row' }}>
+
+                <TouchableOpacity
+                    style={styles.backBox}
+                    onPress={() => navigation.navigate("Home")}
+                >
+                    <Icon name="arrow-back" size={30} />
                 </TouchableOpacity>
+
+                <View style={styles.restaurantTitleBox}>
+                    <View style={styles.restaurantTitle}>
+                        <Text style={styles.restaurantTitleText}>One tum gud wan</Text>
+                    </View>
+                </View>
+
             </View>
-        )        
+        )
     }
 
-	return (
-		<SafeAreaView style={styles.container}>
-			<View style={styles.header} />
-			{renderHeader()}
-		</SafeAreaView>
-	);
+    return (
+        <SafeAreaView style={styles.container}>
+            {renderHeader()}
+        </SafeAreaView>
+    );
 }
 
 export default EateryScreen;
