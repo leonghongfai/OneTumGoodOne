8c7ed003b90c35ca29809b7280488b94312ac5f1
leonghongfai <leonghongfai@gmail.com>
2021-07-24 16:16:24 +0800

    hax

--- src/screens/HomePages/ProfilePageScreen/ProfilePage.js
+++ src/screens/HomePages/ProfilePageScreen/ProfilePage.js
@@ -49,35 +49,7 @@ const ProfilePage = (props) => {
                 })
                 setUserPosts(posts)
             })
-        } else {
-            firebase.firestore()
-                .collection("users")
-                .doc(props.route.params.uid)
-                .get()
-                .then((snapshot) => {
-                    if (snapshot.exists) {
-                        setUser(snapshot.data());
-                        //console.log(snapshot.data())
-                    }
-                    else {
-                        console.log('does not exist')
-                    }
-                })
-            firebase.firestore()
-                .collection("posts")
-                .doc(props.route.params.uid)
-                .collection("userPosts")
-                .orderBy("creation", "asc")
-                .get()
-                .then((snapshot) => {
-                    let posts = snapshot.docs.map(doc => {
-                        const data = doc.data();
-                        const id = doc.id;
-                        return { id, ...data }
-                    })
-                    setUserPosts(posts)
-                })
-        }    
+        }
     }, []);
 
     useEffect(() => {
@@ -201,9 +173,6 @@ const ProfilePage = (props) => {
             })
     }
 
-    if (user === null) {
-        return <View />;
-    }
     return (
         <View style={styles.container}>
             <View style={styles.containerInfo}>
